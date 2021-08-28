8ff31c2cdd97155a9d64dc37a701ed7a0e71dc1f
chocomilku <andrei.berja078@gmail.com>
2021-08-28 17:14:59 +0800

    changed solution to prepend 0 to numbers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 const timer = document.querySelector('.timer');
 const description = document.querySelector('.description');
-const spanDay = document.querySelector('.day')
-const spanHour = document.querySelector('.hour')
-const spanMinute = document.querySelector('.minute')
-const spanSecond = document.querySelector('.second')
 
 let countTo = 'Sep 1, 2021 00:00:00';
 
@@ -13,34 +9,12 @@ function run() {
     const now = new Date().getTime();
     const difference = target.getTime() - now
 
-    let day = Math.floor(difference / (1000 * 60 * 60 *24))
-    let hour = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    let minute = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    let second = Math.floor((difference % (1000 * 60)) / (1000))
+    let day = Math.floor(difference / (1000 * 60 * 60 *24)).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping:false})
+    let hour = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping:false})
+    let minute = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping:false})
+    let second = Math.floor((difference % (1000 * 60)) / (1000)).toLocaleString('en-us', {minimumIntegerDigits: 2, useGrouping:false})
 
-    if (day < 10) {
-        spanDay.innerHTML = '0' + day + 'd'
-    } else {
-        spanDay.innerHTML = day + 'd'
-    }
-
-    if (hour < 10) {
-        spanHour.innerHTML = '0' + hour + 'h'
-    } else {
-        spanHour.innerHTML = hour +'h'
-    }
-    
-    if (minute < 10) {
-        spanMinute.innerHTML = '0' + minute + 'm'
-    } else {
-        spanMinute.innerHTML = minute + 'm'
-    }
-
-    if (second < 10) {
-        spanSecond.innerHTML = '0' + second + 's'
-    } else {
-        spanSecond.innerHTML = second + 's'
-    }
+    timer.innerHTML = `${day}d ${hour}h ${minute}m ${second}s`
 
     description.innerHTML = `Counts until ${countString}`
 }
